46726bf53844e2d4c98edba884ffcf5f5652ec9d
omartins-zs <gabrielmatheus209@gmail.com>
2026-03-26 23:46:05 -0300

    :sparkles: feat: criar model blocked time

---
+++ app/Models/BlockedTime.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Concerns\BelongsToCompany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BlockedTime extends Model
+{
+    use BelongsToCompany;
+    use HasFactory;
+
+    protected $fillable = [
+        'company_id',
+        'starts_at',
+        'ends_at',
+        'reason',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'starts_at' => 'datetime',
+            'ends_at' => 'datetime',
+        ];
+    }
+}

c29981bed293c95eb5ece9b3864b328113883bb6
omartins-zs <gabrielmatheus209@gmail.com>
2026-03-26 23:46:18 -0300

    :card_file_box: data: criar migration add

---
+++ database/migrations/2026_03_22_130631_add_company_foreign_to_users_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->index(['company_id', 'role']);
+            $table->index(['company_id', 'status']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropIndex(['company_id', 'role']);
+            $table->dropIndex(['company_id', 'status']);
+        });
+    }
+};

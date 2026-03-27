0f75ae85d2c017f916a92bd616f175bd1deb15c1
omartins-zs <gabrielmatheus209@gmail.com>
2026-03-26 23:46:44 -0300

    :sparkles: feat: iniciar frontend app

---
+++ resources/js/app.tsx
@@ -0,0 +1,28 @@
+import '../css/app.css';
+import './bootstrap';
+
+import { createInertiaApp } from '@inertiajs/react';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { createRoot } from 'react-dom/client';
+import { resolveAndApplyTheme } from '@/lib/theme';
+
+const appName = import.meta.env.VITE_APP_NAME || 'Agendify';
+
+resolveAndApplyTheme();
+
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) =>
+        resolvePageComponent(
+            `./Pages/${name}.tsx`,
+            import.meta.glob('./Pages/**/*.tsx'),
+        ),
+    setup({ el, App, props }) {
+        const root = createRoot(el);
+
+        root.render(<App {...props} />);
+    },
+    progress: {
+        color: '#0891b2',
+    },
+});
